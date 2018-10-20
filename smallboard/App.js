1ee3d55f8f7ed11cd312725c9ead86a89a07241b
refai1 <refai241@gmail.com>
2018-10-19 23:54:00 -0700

    completed design, minimal functionality.

--- smallboard/App.js
+++ smallboard/App.js
@@ -1,23 +1,116 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import TextButton from './components/TextButton';
+
+
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    
+    state = {
+        entered: "HI!",
+    };
+
+
+    handleAPress = () => {
+        
+        this.setState(prevState => {
+            const {entered} = prevState;
+
+            return {
+                entered: prevState["entered"] + 'a'
+            };
+        });
+    };
+
+
+    render() {
+        const {entered} = this.state;
+        return (
+        <View style={styles.container}>
+
+            <View style={styles.topBar}></View>
+
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>smallboard</Text>
+            </View>
+            
+            <View style={styles.container2}>
+                
+                <Text style={styles.enteredText}>{entered}</Text>
+                
+                <View style={styles.keyboardContainer}>
+                    
+                    <View style={styles.rowContainer}>
+                        <TextButton small color='#ffffff' title='ab' onPress={this.handleAPress()}/>
+                        <TextButton small color='#ffffff' title='efg'/>
+                        <TextButton small color='#ffffff' title='kl'/>
+                    </View>
+
+                    <View style={styles.rowContainer}>
+                        <TextButton small color='#ffffff' title='cd'/>
+                        <TextButton small color='#ffffff' title='hij'/>
+                        <TextButton small color='#ffffff' title='mn'/>
+                    </View>
+                    
+                    <View style={styles.rowContainer}>
+                        <TextButton small color='#ffffff' title='op'/>
+                        <TextButton small color='#ffffff' title='tu'/>
+                        <TextButton small color='#ffffff' title='wx'/>
+                    </View>
+                    
+                    <View style={styles.rowContainer}>
+                        <TextButton small color='#ffffff' title='qrs'/>
+                        <TextButton small color='#ffffff' title='v'/>
+                        <TextButton small color='#ffffff' title='yz'/>
+                    </View>
+
+                </View>
+            </View>
+        </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    container2: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    topBar: {
+        height:20,
+    },    
+    titleContainer: {
+        paddingTop: 2,
+        paddingBottom: 2,
+        borderBottomWidth: 1,
+        borderBottomColor: '#D6D7DA',
+        backgroundColor: "white",
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    enteredText: {
+        fontSize: 18,
+        textAlign: 'center',
+    },
+    keyboardContainer: {
+        backgroundColor: "#9e0000",
+        justifyContent: 'space-around',
+        height: 150,
+        width: 150, 
+    },
+    rowContainer: {
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+
+    },
 });
---
+++ smallboard/components/TextButton.js
@@ -0,0 +1,64 @@
+import { StyleSheet, Text, TouchableOpacity, ColorPropType } from 'react-native';
+
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+
+export default function TextButton({ color, title, small, onPress }){
+	return (
+		<TouchableOpacity
+			style={[styles.button, {borderColor: color}]}
+			onPress={onPress}
+		>
+			<Text
+				style={[
+					styles.buttonText,
+					small ? styles.small : styles.large,
+					{ color },
+				]}
+			>
+				{title}
+			</Text>
+		</TouchableOpacity>
+	);
+}
+
+TextButton.propTypes = {
+ 	color: ColorPropType.isRequired,
+  	title: PropTypes.string.isRequired,
+ 	small: PropTypes.bool,
+  	onPress: PropTypes.func,
+};
+
+TextButton.defaultProps = {
+  	small: false,
+};
+
+const styles = StyleSheet.create({
+	button: {
+		marginTop: 5,
+		minWidth: 10,
+		height: 30,
+		borderWidth: 2,
+		borderRadius: 3,
+	},
+	small: {
+		fontSize: 14,
+		padding: 5
+	},
+	large: {
+		fontSize: 16,
+		padding: 20,
+		margin: 10
+	},
+	buttonText: {
+		textAlign: 'center',
+		fontWeight: 'bold',
+	},
+	title: {
+		fontSize: 14,
+		fontWeight: 'bold',
+	},
+});
+
